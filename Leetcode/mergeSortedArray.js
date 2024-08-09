432c07bc6e7643b3ccfe9b772e533d17caa3f537
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2024-08-08 21:13:53 -0400

    reorganizing

--- Leetcode/mergeSortedArray.js
+++ Leetcode/mergeSortedArray.js
@@ -1,35 +1,35 @@
-// Test Case
-let nums1 = [1, 2, 3, 0, 0, 0]
-let m = 3
-let nums2 = [2, 5, 6]
-let n = 3
-
 // Function
 var merge = function (nums1, m, nums2, n) {
-    // Start from the end of nums1 and nums2
-    let i = m - 1; // Last element in the initial part of nums1
-    let j = n - 1; // Last element in nums2
-    let k = m + n - 1; // Last position in nums1
+  // Start from the end of nums1 and nums2
+  let i = m - 1; // Last element in the initial part of nums1
+  let j = n - 1; // Last element in nums2
+  let k = m + n - 1; // Last position in nums1
 
-    // Merge in reverse order
-    while (i >= 0 && j >= 0) {
-        if (nums1[i] > nums2[j]) {
-            nums1[k] = nums1[i];
-            i--;
-        } else {
-            nums1[k] = nums2[j];
-            j--;
-        }
-        k--;
+  // Merge in reverse order
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
     }
+    k--;
+  }
 
-    // If there are remaining elements in nums2, copy them
-    while (j >= 0) {
-        nums1[k] = nums2[j];
-        j--;
-        k--;
-    }
+  // If there are remaining elements in nums2, copy them
+  while (j >= 0) {
+    nums1[k] = nums2[j];
+    j--;
+    k--;
+  }
 };
+
+// Test Case
+let nums1 = [1, 2, 3, 0, 0, 0];
+let m = 3;
+let nums2 = [2, 5, 6];
+let n = 3;
 
 merge(nums1, m, nums2, n);
 console.log(nums1);
