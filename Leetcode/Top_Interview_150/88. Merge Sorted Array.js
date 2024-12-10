1f5ce7804c22a0473255ea921e01d38941b7b480
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2024-12-10 10:59:31 -0500

    input arrays and parameters

--- Leetcode/Top_Interview_150/88. Merge Sorted Array.js
+++ Leetcode/Top_Interview_150/88. Merge Sorted Array.js
@@ -1,4 +1,4 @@
-var merge = function(nums1, m, nums2, n) {
+function merge(nums1, m, nums2, n) {
     // Combine the first m elements of nums1 with all elements of nums2
     let combined = nums1.slice(0, m).concat(nums2);
 
@@ -10,3 +10,13 @@ var merge = function(nums1, m, nums2, n) {
         nums1[i] = combined[i];
     }
 };
+
+// Input arrays and paramters
+let nums1 = [1, 2, 3, 0, 0, 0];
+let m = 3;
+let nums2 = [2, 3, 5, 6]
+let n = 4;
+
+merge(nums1, m, nums2, n);
+console.log(nums1);
+
