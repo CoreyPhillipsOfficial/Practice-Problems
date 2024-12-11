09039e0cf2458889727d2a8bc65ce20f92c95193
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2024-12-11 12:45:22 -0500

    experimenting with merge function

--- Leetcode/Top_Interview_150/88. Merge Sorted Array.js
+++ Leetcode/Top_Interview_150/88. Merge Sorted Array.js
@@ -17,6 +17,57 @@ let m = 3;
 let nums2 = [2, 3, 5, 6]
 let n = 4;
 
-merge(nums1, m, nums2, n);
+merge(nums1, m, nums2, n)
 console.log(nums1);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function merge(nums1, m, nums2, n) {
+//     // form a new array
+//     let combined = nums1.slice(0, m).concat(nums2)
+
+//     // sort new array
+//     combined.sort((a, b) => a-b)
+
+//     // make nums1 same as new array
+
+//     return combined;
+// }
+
+// // Input arrays and paramters
+// let nums1 = [1, 2, 3, 0, 0, 0];
+// let m = 3;
+// let nums2 = [2, 3, 5, 6]
+// let n = 4;
+
+// console.log(merge(nums1, m, nums2, n));
+
+
+
+// let unsortedArray = [3, 6, 4, 10, 2, 44]
+
+// function sortArray() {
+//     return unsortedArray.sort((a, b) => b - a);
+// }
+
+// console.log(sortArray(unsortedArray));
+
+
+// let letterArray = ['w', 'b', 'r', 'h', 'a']
+
+// console.log(letterArray.sort());
