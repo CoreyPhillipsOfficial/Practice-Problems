5bc34b9800f9780fefd1b5162d89e6bc48158cd6
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2024-12-14 15:02:01 -0500

    correct solution

--- Leetcode/Top_Interview_150/27. Remove Element.js
+++ Leetcode/Top_Interview_150/27. Remove Element.js
@@ -1,19 +1,11 @@
 // https://leetcode.com/problems/remove-element/description/?envType=study-plan-v2&envId=top-interview-150
 var removeElement = function(nums, val) {
-    // loop through elements of array and remove val
-    for (i = 0; i < nums.length; i++) {
-        // check if i=k
-        if (nums[i] === k) {
-            nums[i] = "_"
+    let k = 0; // Initialize the counter for non-val elements
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) { // Check if the current element is not equal to val
+            nums[k] = nums[i]; // Place it at the k-th position
+            k++; // Increment the counter
         }
     }
+    return k; // Return the count of non-val elements
 };
-
-
-
-
-
-
-
-
-
