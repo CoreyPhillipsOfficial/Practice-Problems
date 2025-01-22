1ac5f46329da1c64b5c54f34884049abe3acd80f
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2025-01-22 17:07:14 -0500

    getTotal function

---
+++ FrontendPrep/getTotal.js
@@ -0,0 +1,17 @@
+// Write a javascript function to get the total of given arguments like total(2,3,4,5)(4,5,6)
+
+function getTotal(...args) {
+    let sum = 0;
+    for (let i = 0; i < args.length; i++) {
+        sum += args[i]
+    }
+
+    return function (...ar) {
+        for (let i = 0; i < ar.length; i++) {
+            sum += ar[i]
+        }
+        return sum;
+    }
+}
+
+console.log(getTotal(2, 3, 4, 5)(4, 5, 6)); // Output: 29
